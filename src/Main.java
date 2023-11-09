e41103947a7053660bb2224a387936883a575bc9
fmtk <fiadmtk@protonmail.com>
2023-11-09 00:19:23 -0500

    boom

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        // setting doors
+        int goat1;
+        int goat2;
+        int car;
+        car = (int) (Math.random() * (3 - 1 + 1)) + 1;
+        if (car == 1) {
+            goat1 = (int) (Math.random() * (3 - 2 + 1)) + 2;
+            if (goat1 == 2) {goat2 = 3;} else {goat2 = 2;}
+        } if (car == 2) {
+            int goat1Choice2 = (int) (Math.random() * (2 - 1 + 1)) + 1; // will later decide of goat1 is 1 or 3
+            if (goat1Choice2 == 1) {goat1 = 1;} else {goat1 = 3;}
+            if (goat1 == 1) {goat2 = 3;} else {goat2 = 1;}
+        } else  {
+            int goat1Choice3 = (int) (Math.random() * (2 - 1 + 1)) + 1; // will later decide of goat1 is 1 or 2
+            if (goat1Choice3 == 1) {goat1 = 1;} else {goat1 = 2;}
+            if (goat1 == 1) {goat2 = 2;} else {goat2 = 1;}
+        }
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Monty Hall game!");
+        System.out.println("There are two goats and a car randomly placed behind doors 1, 2, and 3");
+        System.out.print("Where do you think the car is? Enter 1, 2, or 3 -> ");
+        int userGuess = sc.nextInt();
+        sc.nextLine();
+        MontyHall monty = new MontyHall(goat1, goat2, car, userGuess);
+        System.out.println("One of the goats is behind door number " + monty.checkUserFirstPick(userGuess));
+        System.out.print("Do you want to switch your guess? (Yes/No) -> ");
+        String switchInput = sc.nextLine();
+        monty.checkIfSwitch(switchInput);
+        userGuess = monty.getUserDoorGuess();
+
+        if (monty.checkUserEqualCar(userGuess)) {
+            System.out.println("The car is behind door " + car + ". You won!");
+        } else {
+            System.out.println("The car is behind door " + car + ". You lost :( (Or you could say you won a goat!)");
+        }
+    }
 }
--- src/MontyHall.java
+++ src/MontyHall.java
@@ -1,2 +1,47 @@
 public class MontyHall {
+    private int goat1;
+    private int goat2;
+    private int car;
+    private int revealedGoatDoor;
+    private int userDoorGuess;
+
+
+    public MontyHall(int goat1, int goat2, int car, int userDoorGuess) {
+        this.goat1 = goat1;
+        this.goat2 = goat2;
+        this.car = car;
+        this.userDoorGuess = userDoorGuess;
+    }
+
+    public int getUserDoorGuess() {
+        return userDoorGuess;
+    }
+
+    public int checkUserFirstPick(int userDoorGuess) {
+        if (userDoorGuess == car) {
+            int randomGoatDoorReturn = (int) (Math.random() * (2 - 1 + 1)) + 1;
+            if (randomGoatDoorReturn == 1) {revealedGoatDoor = goat1;} else {revealedGoatDoor = goat2;}
+        } else if (userDoorGuess == goat1) {
+            revealedGoatDoor = goat2;
+        } else if (userDoorGuess == goat2) {
+            revealedGoatDoor = goat1;}
+        return revealedGoatDoor;
+    }
+
+    public void checkIfSwitch(String switchChoice) {
+        if (switchChoice.equals("Yes")) {
+            if ((userDoorGuess == 1 && revealedGoatDoor == 2) || (userDoorGuess == 2 && revealedGoatDoor == 1)) {
+                userDoorGuess = 3;
+            } else if ((userDoorGuess == 1 && revealedGoatDoor == 3) || (userDoorGuess == 3 && revealedGoatDoor == 1)) {
+                userDoorGuess = 2;
+            } else if ((userDoorGuess == 2 && revealedGoatDoor == 3) || (userDoorGuess == 3 && revealedGoatDoor == 2)) {
+                userDoorGuess = 1;
+            }
+        }
+    }
+
+
+    public boolean checkUserEqualCar(int userDoorGuess) {
+        return userDoorGuess == car;
+    }
 }
